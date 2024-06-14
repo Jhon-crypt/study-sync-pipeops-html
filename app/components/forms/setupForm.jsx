ade6f1735bbe81c370c9eea9db27fda5e5f6189d
Jhon-crypt <johnoladele690@gmail.com>
2024-06-14 18:24:24 +0100

    feat: Updated set up formdata

--- app/components/forms/setupForm.jsx
+++ app/components/forms/setupForm.jsx
@@ -11,68 +11,103 @@ export default function SetupForm() {
 
     const [loading, setLoading] = useState(false)
 
-    //const router = useRouter()
+    const [additionalData, setAdditionalData] = useState({
+        country: '',
+        gender: '',
+        institution: '',
+        gradePoint: '',
+    });
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setAdditionalData((prevData) => ({
+            ...prevData,
+            [name]: value,
+        }));
+    };
+
+    const router = useRouter()
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(additionalData)
+
+        // Retrieve existing form data from localStorage
+        const existingFormData = JSON.parse(localStorage.getItem('formData')) || {};
+
+        // Merge the additional data with the existing form data
+        const updatedFormData = { ...existingFormData, ...additionalData };
+
+        // Save the updated form data back to localStorage
+        localStorage.setItem('formData', JSON.stringify(updatedFormData));
+        console.log("Form data updated:", updatedFormData.email);
+    };
 
     return (
 
         <>
 
 
-            <form>
+            <form onSubmit={handleSubmit}>
                 <div class="mb-4">
                     <label class="form-label" style={{ fontSize: "16px", fontFamily: "Fredoka, sans-serif", fontWeight: '500' }}>Country</label>
-                    <select class="form-select" aria-label="Default select example" style={{ backgroundColor: "#F7F2F6", height: "44px", borderRadius: "10px" }}>
+                    <select class="form-select" aria-label="Default select example" style={{ backgroundColor: "#F7F2F6", height: "44px", borderRadius: "10px" }}
+                        name="country"
+                        value={additionalData.country}
+                        onChange={handleChange}
+                    >
                         <option selected><span style={{ fontFamily: "Fredoka, sans-serif", fontSize: "10px" }}>e.g Nigeria</span></option>
-                        <option value="1">One</option>
-                        <option value="2">Two</option>
-                        <option value="3">Three</option>
+                        <option value="Nigeria">Nigeria</option>
                     </select>
                 </div>
 
-                <div class="mb-4">
-                    <label class="form-label" style={{ fontSize: "16px", fontFamily: "Fredoka, sans-serif", fontWeight: '500' }}>City</label>
-                    <select class="form-select" aria-label="Default select example" style={{ backgroundColor: "#F7F2F6", height: "44px", borderRadius: "10px" }}>
-                        <option selected><span style={{ fontFamily: "Fredoka, sans-serif", fontSize: "10px" }}>e.g Lagos</span></option>
-                        <option value="1">One</option>
-                        <option value="2">Two</option>
-                        <option value="3">Three</option>
-                    </select>
-                </div>
+
 
                 <div class="mb-4">
                     <label class="form-label" style={{ fontSize: "16px", fontFamily: "Fredoka, sans-serif", fontWeight: '500' }}>Gender</label>
-                    <select class="form-select" aria-label="Default select example" style={{ backgroundColor: "#F7F2F6", height: "44px", borderRadius: "10px" }}>
+                    <select class="form-select" aria-label="Default select example" style={{ backgroundColor: "#F7F2F6", height: "44px", borderRadius: "10px" }}
+                        name="gender"
+                        value={additionalData.gender}
+                        onChange={handleChange}
+                    >
                         <option selected><span style={{ fontFamily: "Fredoka, sans-serif", fontSize: "10px" }}>e.g Female</span></option>
-                        <option value="1">One</option>
-                        <option value="2">Two</option>
-                        <option value="3">Three</option>
+                        <option value="male">Male</option>
+                        <option value="female">Female</option>
                     </select>
                 </div>
 
                 <div class="mb-4">
                     <label class="form-label" style={{ fontSize: "16px", fontFamily: "Fredoka, sans-serif", fontWeight: '500' }}>Name Of Institution</label>
-                    <select class="form-select" aria-label="Default select example" style={{ backgroundColor: "#F7F2F6", height: "44px", borderRadius: "10px" }}>
+                    <select class="form-select" aria-label="Default select example" style={{ backgroundColor: "#F7F2F6", height: "44px", borderRadius: "10px" }}
+                        name="institution"
+                        value={additionalData.institution}
+                        onChange={handleChange}
+                    >
                         <option selected><span style={{ fontFamily: "Fredoka, sans-serif", fontSize: "10px" }}>e.g Olabisi Onabanjo University</span></option>
-                        <option value="1">One</option>
-                        <option value="2">Two</option>
-                        <option value="3">Three</option>
+                        <option value="tasued">Tasued</option>
+                        <option value="oou">OOU</option>
+                        <option value="unilag">Unilag</option>
                     </select>
                 </div>
 
                 <div class="mb-4">
                     <label class="form-label" style={{ fontSize: "16px", fontFamily: "Fredoka, sans-serif", fontWeight: '500' }}>University Grade Point</label>
-                    <select class="form-select" aria-label="Default select example" style={{ backgroundColor: "#F7F2F6", height: "44px", borderRadius: "10px" }}>
+                    <select class="form-select" aria-label="Default select example" style={{ backgroundColor: "#F7F2F6", height: "44px", borderRadius: "10px" }}
+                        name="gradePoint"
+                        value={additionalData.gradePoint}
+                        onChange={handleChange}
+                    >
                         <option selected><span style={{ fontFamily: "Fredoka, sans-serif", fontSize: "10px" }}>e.g 5.0, 7.0</span></option>
-                        <option value="1">One</option>
-                        <option value="2">Two</option>
-                        <option value="3">Three</option>
+                        <option value="4.0">4.0</option>
+                        <option value="5.0">5.0</option>
+                        <option value="7.0">7.0</option>
                     </select>
                 </div>
 
                 <div className="mt-5 mb-3 d-grid">
-                    <Link href="/dashboard" className="btn btn-block border-0 text-white px-5 py-2" style={{ fontFamily: "Fredoka, sans-serif", background: "linear-gradient(to right, #D95388, #85486e)" }}>
+                    <button type="submit" className="btn btn-block border-0 text-white px-5 py-2" style={{ fontFamily: "Fredoka, sans-serif", background: "linear-gradient(to right, #D95388, #85486e)" }}>
                         Sign Up
-                    </Link>
+                    </button>
                 </div>
             </form>
 
