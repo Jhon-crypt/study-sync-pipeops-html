d99a0040761e8c0c76c203d5d6b44abf587cc3db
Jhon-crypt <johnoladele690@gmail.com>
2024-06-14 20:58:32 +0100

    updated signin api route

--- app/api/auth/signin/route.js
+++ app/api/auth/signin/route.js
@@ -1,5 +1,6 @@
 import { NextResponse } from "next/server";
 import { headers } from 'next/headers';
+import { writeFile } from 'fs/promises'; // Use promises for async/await
 
 
 export async function POST(req) {
@@ -19,16 +20,24 @@ export async function POST(req) {
     // Check if the Bearer token matches the expected value from the environment variables
     if (bearer_token === process.env.MASTER_BEARER_KEY) {
 
-       
-
-        const formData = await req.formData();
 
 
-            
-            return NextResponse.json({ formData });
+        const json = await req.json();
+
+        const base64Data = json.image; // Assuming the base64 image is in json.image
+
+        const base64String = base64Data.replace(/^data:image\/\w+;base64,/, "");
 
 
-        
+        const buffer = Buffer.from(base64String, 'base64');
+
+        // Define the path where the image will be saved
+        const filePath = 'uploaded.png';
+
+        // Write the file
+        await writeFile(filePath, buffer);
+
+        return NextResponse.json({ message: "Image saved successfully" });
 
     }
 
