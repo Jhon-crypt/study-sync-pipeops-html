d176d78bb939ae7d33b1aef0b4c0011f733ac74d
Jhon-crypt <johnoladele690@gmail.com>
2024-06-15 11:56:26 +0100

    feat: Updated signin api

--- app/api/auth/signin/route.js
+++ app/api/auth/signin/route.js
@@ -4,6 +4,7 @@ import bcrypt from "bcrypt";
 import { cookies } from 'next/headers'
 //import { writeFile } from 'fs/promises'; // Use promises for async/await
 import supabase from "@/app/config/supabase";
+import { v4 as uuidv4 } from 'uuid';
 
 
 export async function POST(req) {
@@ -39,7 +40,7 @@ export async function POST(req) {
         // Checking if the user exists
         const { data, error } = await supabase
             .from('users')
-            .select('email, password')
+            .select('email, password, user_id')
             .eq('email', signin_data.email)
             .single();
 
@@ -50,14 +51,52 @@ export async function POST(req) {
             const user = data;
 
             if (bcrypt.compareSync(signin_data.password, user.password) == true) {
-                const encryptedSessionData = 12345; // Encrypt your session data
-                cookies().set('session', encryptedSessionData, {
+
+                const session_id = uuidv4();
+
+                cookies().set('sync-session', session_id, {
                     httpOnly: true,
-                    secure: process.env.NODE_ENV === 'production',
-                    maxAge: 5, // 5 seconds
+                    secure: false,
+                    maxAge: 60 * 60 * 24 * 7, // One week
                     path: '/',
-                });
-                return NextResponse.json({ message: "User Logged in" }, { status: 200 });
+                })
+
+                //check if user session exists
+                const { data, error } = await supabase
+                    .from('sessions')
+                    .select('user_id')
+                    .eq('user_id', user.user_id)
+                    .single();
+
+                if (error) {
+                    //create new session
+
+                    const { error } = await supabase
+                        .from("sessions")
+                        .insert({
+                            "session_id": session_id,
+                            "user_id": user.user_id,
+                            "status": "Active",
+                        });
+                    if (error) {
+                        return NextResponse.json({ message: error }, { status: 500 });
+                    } else {
+                        return NextResponse.json({ message: "successfully Logged in", data: {user_id : user.user_id} }, { status: 200 });
+                    }
+                } else {
+                    //update existing session
+                    const { data, error } = await supabase
+                        .from('sessions')
+                        .update({ "session_id": session_id})
+                        .eq('user_id', user.user_id)
+                        .select()
+                    if (error) {
+                        return NextResponse.json({ message: error }, { status: 500 });
+                    } else {
+                        return NextResponse.json({ message: "successfully Logged in", data: {user_id : user.user_id} }, { status: 200 });
+                    }
+                }
+
             } else {
                 return NextResponse.json({ message: "User Not Found" }, { status: 500 });
             }
