4bbdc17ab2e1e6db29cef3f660944f0fcfe01a80
Jhon-crypt <johnoladele690@gmail.com>
2024-06-19 10:42:09 +0100

    feat:Integrated gemini 1.5-pro with study plan api

--- app/intro/page.js
+++ app/intro/page.js
@@ -1,6 +1,18 @@
+"use client"
 import Link from "next/link"
+import { useMediaQuery } from 'react-responsive'
 
 export default function Page() {
+
+    const Desktop = ({ children }) => {
+        const isDesktop = useMediaQuery({ minWidth: 992 })
+        return isDesktop ? children : null
+    }
+
+    const Mobile = ({ children }) => {
+        const isMobile = useMediaQuery({ maxWidth: 767 })
+        return isMobile ? children : null
+    }
 
     return (
 
@@ -8,18 +20,47 @@ export default function Page() {
 
             <div>
                 <div className="container-fluid text-center">
-                    <nav class="navbar bg-body-tertiary">
+                    <nav class="navbar navbar-expand-lg bg-body-tertiary">
                         <div class="container-fluid">
                             <a class="navbar-brand" href="#">
                                 <img src="/logo2.png" alt="StudySync Logo" className="img-fluid" width={160} />
                             </a>
+                            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                                <span class="navbar-toggler-icon"></span>
+                            </button>
+                            <div class="collapse navbar-collapse" id="navbarNav">
+                                <Mobile>
+                                    <ul class="navbar-nav">
+                                        <li class="nav-item">
+                                            <Link class="nav-link active" href="/signin">Signin</Link>
+                                        </li>
+                                        <li class="nav-item">
+                                            <Link class="nav-link" href="/signup">Signup</Link>
+                                        </li>
+                                       
+                                    </ul>
+                                </Mobile>
+                            </div>
+                            <Desktop>
+                                <div>
+                                    <button className="btn me-2" style={{ border: "1px solid #B670E3", fontFamily: "Fredoka, sans-serif" }}>Sign In</button>
+                                    <button className="btn text-white" style={{ background: "linear-gradient(to right, #B670E3, #E864DB)", fontFamily: "Fredoka, sans-serif" }}>Sign up</button>
+                                </div>
+                            </Desktop>
+                        </div>
+                    </nav>
+                    {/*}
+                    <nav class="navbar bg-body-tertiary">
+                        <div class="container-fluid">
+
                             <div>
                                 <button className="btn me-2" style={{ border: "1px solid #B670E3", fontFamily: "Fredoka, sans-serif" }}>Sign In</button>
                                 <button className="btn text-white" style={{ background: "linear-gradient(to right, #B670E3, #E864DB)", fontFamily: "Fredoka, sans-serif" }}>Sign up</button>
                             </div>
                         </div>
                     </nav>
-                   
+                    {*/}
+
                     <br />
                     <div className="row align-items-center">
                         <div className="col-md-6 text-md-start text-center">
